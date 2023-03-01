0dbfafe1336c17b17b83d72cc0cc2c14e2fea072
Vilsol <me@vil.so>
2023-02-28 20:22:32 -0500

    use system-compiled lib

--- ubus-sys/build.rs
+++ ubus-sys/build.rs
@@ -1,60 +1,15 @@
 use std::env;
-use std::env::consts;
-use std::path::{Path, PathBuf};
+use std::path::{PathBuf};
 
 fn main() {
-    if let Err(_) = pkg_config::probe_library("ubus") {
-        if let Some(lib_path) = env::var_os("LIBUBUS_DIR") {
-            // I have no idea if this branch works
-            let lib_dir = Path::new(&lib_path);
-            let dylib_name = format!("{}ubus{}", consts::DLL_PREFIX, consts::DLL_SUFFIX);
-            if lib_dir.join(dylib_name).exists()
-                || lib_dir.join("libubus.a").exists()
-                || lib_dir.join("ubus.lib").exists()
-            {
-                println!(
-                    "cargo:rustc-link-search=native={}",
-                    lib_path.into_string().unwrap()
-                );
-                println!("cargo:rustc-link-lib=ubus");
-            }
-        } else {
-            let json_c = cmake::build("vendor/json-c");
-
-            cc::Build::new()
-                .file("vendor/ubus/libubus.c")
-                .file("vendor/ubus/libubus-req.c")
-                .file("vendor/ubus/libubus-io.c")
-                .file("vendor/ubus/libubus-obj.c")
-                .file("vendor/ubus/libubus-sub.c")
-                .file("vendor/libubox/avl.c")
-                .file("vendor/libubox/utils.c")
-                .file("vendor/libubox/uloop.c")
-                .file("vendor/libubox/blob.c")
-                .file("vendor/libubox/usock.c")
-                .file("vendor/libubox/blobmsg.c")
-                .file("vendor/libubox/blobmsg_json.c")
-                .include("vendor")
-                .include("vendor/ubus")
-                .include("vendor/libubox")
-                .include(json_c.join("include/json-c"))
-                .define("UBUS_MAX_MSGLEN", "1048576")
-                .define("UBUS_UNIX_SOCKET", "\"/var/run/ubus.sock\"")
-                .define("JSONC", "1")
-                .shared_flag(true)
-                .compile("libubus.so");
-        }
-    }
-
     let bindings = bindgen::Builder::default()
-        .header("vendor/ubus/libubus.h")
-        .header("vendor/libubox/blobmsg_json.h")
-        .clang_args(vec!["-Ivendor", "-target", "mips-unknown-linux"])
-        .whitelist_function("ubus.*")
-        .whitelist_type("ubus.*")
-        .whitelist_var("ubus.*")
-        .whitelist_function("blob.*")
-        .whitelist_var("blob.*")
+        .header("/usr/local/include/libubus.h")
+        .header("/usr/local/include/libubox/blobmsg_json.h")
+        .allowlist_function("ubus.*")
+        .allowlist_type("ubus.*")
+        .allowlist_var("ubus.*")
+        .allowlist_function("blob.*")
+        .allowlist_var("blob.*")
         .generate()
         .expect("Unable to generate bindings");
 
